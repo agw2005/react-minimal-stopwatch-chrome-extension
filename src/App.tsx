d5bb34e95a3f7da1760c101332aac30b40c18b1d
agw2005 <danial.agw2005@gmail.com>
2025-12-07 14:06:04 +0700

    feat(chrome.storage): saves unix epoch to chrome storage API

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [unixEpochMilliseconds, setUnixEpochMilliseconds] = useState<number>(0);
   const setDate = () => {
-    setUnixEpochMilliseconds(new Date().getTime());
+    if (unixEpochMilliseconds === 0) {
+      setUnixEpochMilliseconds(new Date().getTime());
+    } else {
+      setUnixEpochMilliseconds(0);
+      chrome.storage.sync.remove(unixEpochMilliseconds);
+    }
   };
 
-  
+  // Load epoch on render from chrome storage
+  useEffect(() => {
+    chrome.storage.sync.get({ unixEpochMilliseconds: 0 }, (result: any) => {
+      if (chrome.runtime.lastError) {
+        console.error("Error reading storage:", chrome.runtime.lastError);
+      } else {
+        setUnixEpochMilliseconds(result.unixEpochMilliseconds);
+      }
+    });
+  }, []);
+
+  // Save epoch to chrome storage
+  useEffect(() => {
+    chrome.storage.sync.set({ unixEpochMilliseconds }, () => {
+      if (chrome.runtime.lastError) {
+        console.error("Error writing storage:", chrome.runtime.lastError);
+      }
+    });
+  }, [unixEpochMilliseconds]);
 
   return (
     <div className="m-4">
@@ -14,7 +37,7 @@ function App() {
       <p>Unix Epoch : {unixEpochMilliseconds}</p>
       <p>Date Epoch : {new Date(unixEpochMilliseconds).toUTCString()}</p>
       <button type="button" className="p-2 border-2" onClick={setDate}>
-        Set date
+        {unixEpochMilliseconds === 0 ? "Start" : "Stop"}
       </button>
     </div>
   );
