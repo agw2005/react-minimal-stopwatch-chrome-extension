8782d4917ce4f95d2e458c005aa28abe47d29dd9
agw2005 <danial.agw2005@gmail.com>
2025-12-07 13:36:30 +0700

    feat(App.tsx): scaffold simple interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const [unixEpochMilliseconds, setUnixEpochMilliseconds] = useState<number>(0);
+  const setDate = () => {
+    setUnixEpochMilliseconds(new Date().getTime());
+  };
+
+  
+
   return (
     <div className="m-4">
       <h1 className="text-2xl font-bold">Stopwatch</h1>
-      <p>Hello World</p>
+      <p>Unix Epoch : {unixEpochMilliseconds}</p>
+      <p>Date Epoch : {new Date(unixEpochMilliseconds).toUTCString()}</p>
+      <button type="button" className="p-2 border-2" onClick={setDate}>
+        Set date
+      </button>
     </div>
   );
 }
